138a719115fbdd27df6a47c9c4af06c8ff3f41ff
Matt Wayland <mw283@cam.ac.uk>
2022-04-25 12:23:26 +0100

    Update and rename UNIT_LoRaWAN.cpp to M5Stack_UNIT_LoRaWAN.cpp

--- src/M5Stack_UNIT_LoRaWAN.cpp
+++ src/M5Stack_UNIT_LoRaWAN.cpp
@@ -1,4 +1,4 @@
-#include "UNIT_LoRaWAN.h"
+#include "M5Stack_UNIT_LoRaWAN.h"
 
 void UNIT_LoRaWAN::Init(HardwareSerial *serial, uint8_t RX, uint8_t TX) {
     _serial = serial;
@@ -6,7 +6,7 @@ void UNIT_LoRaWAN::Init(HardwareSerial *serial, uint8_t RX, uint8_t TX) {
     _serial->flush();
 }
 
-bool UNIT_LoRaWAN::checkDeviceConnect() {
+bool M5Stack_UNIT_LoRaWAN::checkDeviceConnect() {
     String restr;
     writeCMD("AT+CGMI?\r\n");
     restr = waitMsg(500);
@@ -17,7 +17,7 @@ bool UNIT_LoRaWAN::checkDeviceConnect() {
     }
 }
 
-bool UNIT_LoRaWAN::checkJoinStatus() {
+bool M5Stack_UNIT_LoRaWAN::checkJoinStatus() {
     String restr;
     writeCMD("AT+CSTATUS?\r\n");
     restr = waitMsg(500);
@@ -33,7 +33,7 @@ bool UNIT_LoRaWAN::checkJoinStatus() {
     }
 }
 
-String UNIT_LoRaWAN::waitMsg(unsigned long time) {
+String M5Stack_UNIT_LoRaWAN::waitMsg(unsigned long time) {
     String restr;
     unsigned long start = millis();
     while (1) {
@@ -47,19 +47,19 @@ String UNIT_LoRaWAN::waitMsg(unsigned long time) {
     return restr;
 }
 
-void UNIT_LoRaWAN::writeCMD(String command) {
+void M5Stack_UNIT_LoRaWAN::writeCMD(String command) {
     _serial->print(command);
     delay(100);
 }
 
-void UNIT_LoRaWAN::sendMsg(uint8_t confirm, uint8_t nbtrials, size_t length,
+void M5Stack_UNIT_LoRaWAN::sendMsg(uint8_t confirm, uint8_t nbtrials, size_t length,
                            String data) {
     String cmd = "AT+DTRX=" + String(confirm) + ',' + String(nbtrials) + ',' +
                  String(length) + ',' + data + "\r\n";
     writeCMD(cmd);
 }
 
-String UNIT_LoRaWAN::receiveMsg() {
+String M5Stack_UNIT_LoRaWAN::receiveMsg() {
     String restr = waitMsg(2000);
     if (restr.indexOf("OK+RECV:") != -1 && restr.indexOf("02,00,00") == -1) {
         String data = restr.substring(restr.indexOf("OK+RECV:") + 17, -2);
@@ -69,7 +69,7 @@ String UNIT_LoRaWAN::receiveMsg() {
     }
 }
 
-void UNIT_LoRaWAN::configOTTA(String device_eui, String app_eui, String app_key,
+void M5Stack_UNIT_LoRaWAN::configOTTA(String device_eui, String app_eui, String app_key,
                               String ul_dl_mode) {
     writeCMD("AT+CJOINMODE=0\r\n");
     writeCMD("AT+CDEVEUI=" + device_eui + "\r\n");
@@ -78,7 +78,7 @@ void UNIT_LoRaWAN::configOTTA(String device_eui, String app_eui, String app_key,
     writeCMD("AT+CULDLMODE=" + ul_dl_mode + "\r\n");
 }
 
-void UNIT_LoRaWAN::configABP(String device_addr, String app_skey,
+void M5Stack_UNIT_LoRaWAN::configABP(String device_addr, String app_skey,
                              String net_skey, String ul_dl_mode) {
     writeCMD("AT+CJOINMODE=1\r\n");
     writeCMD("AT+CDEVADDR=" + device_addr + "\r\n");
@@ -87,18 +87,18 @@ void UNIT_LoRaWAN::configABP(String device_addr, String app_skey,
     writeCMD("AT+CULDLMODE=" + ul_dl_mode + "\r\n");
 }
 
-void UNIT_LoRaWAN::setClass(String mode) {
+void M5Stack_UNIT_LoRaWAN::setClass(String mode) {
     writeCMD("AT+CCLASS=" + mode + "\r\n");
 }
 
-void UNIT_LoRaWAN::setRxWindow(String freq) {
+void M5Stack_UNIT_LoRaWAN::setRxWindow(String freq) {
     writeCMD("AT+CRXP=0,0," + freq + "\r\n");
 }
 
-void UNIT_LoRaWAN::setFreqMask(String mask) {
+void M5Stack_UNIT_LoRaWAN::setFreqMask(String mask) {
     writeCMD("AT+CFREQBANDMASK=" + mask + "\r\n");
 }
 
-void UNIT_LoRaWAN::startJoin() {
+void M5Stack_UNIT_LoRaWAN::startJoin() {
     writeCMD("AT+CJOIN=1,0,10,8\r\n");
 }
